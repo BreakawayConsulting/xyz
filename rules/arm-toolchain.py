cb2db952d3a6c651dac5a285e8b0dc01a5e12a4b
Benno Leslie <benno@breakawayconsulting.com.au>
2013-01-14 17:33:31 +1100

    Add gdb to the toolchain

--- rules/arm-toolchain.py
+++ rules/arm-toolchain.py
@@ -5,6 +5,8 @@ class ArmToolchain(xyz.BuildProtocol):
     pkg_name = 'arm-toolchain'
     supported_targets = ['arm-none-eabi']
     deps = [('gcc', {'target': 'arm-none-eabi'}),
-            ('binutils', {'target': 'arm-none-eabi'})]
+            ('binutils', {'target': 'arm-none-eabi'}),
+            ('gdb', {'target': 'arm-none-eabi'})
+            ]
 
 rules = ArmToolchain()
