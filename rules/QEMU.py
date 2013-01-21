123f3e706e1931120e035acd2f11fcb1fa3320c7
Benno Leslie <benno@breakawayconsulting.com.au>
2013-01-21 21:09:46 +1100

    Remove unnecessary files

--- rules/QEMU.py
+++ rules/QEMU.py
@@ -1,10 +1,11 @@
 import xyz
+import shutil
 
 class Qemu(xyz.BuildProtocol):
     pkg_name = 'QEMU'
     deps = ['pkg-config', 'gettext', 'glib']
 
-    def configure(self, builder, config):
+    def configure(self):
         env = {}
         ldflags = '{standard_ldflags} -F/Library/Frameworks -F/System/Library/Frameworks'
         args = ('{source_dir_from_build}/configure',
@@ -32,12 +33,31 @@ class Qemu(xyz.BuildProtocol):
                 )
         base_env = {
             'LDFLAGS': ldflags,
-            'PKG_CONFIG_PATH': '{devtree_dir_abs}/{host}/lib/pkgconfig'.format(**config),
-            'QEMU_PKG_CONFIG_FLAGS': '--define-variable prefix={devtree_dir_abs} --define-variable exec_prefix={devtree_dir_abs}/{host}'.format(**config)
+            'PKG_CONFIG_PATH': '{devtree_dir_abs}/{host}/lib/pkgconfig'.format(**self.config),
+            'QEMU_PKG_CONFIG_FLAGS': '--define-variable prefix={devtree_dir_abs} --define-variable exec_prefix={devtree_dir_abs}/{host}'.format(**self.config)
                     }
         base_env.update(env)
-        builder.cmd(*args, env=base_env, config=config)
+        self.builder.cmd(*args, env=base_env, config=self.config)
 
+    def install(self):
+        super().install()
+        # Now we go and remove all the stuff we don't want.
+        # In fact, it may be easy to just install manually what we do want, but
+        # to try and keep this working for future version we take this
+        # approach for now.
 
+        keymaps_dir = self.builder.j('{install_dir}', self.config['prefix'][1:], 'share', 'qemu',
+                                     config=self.config)
+        xyz.rmtree(keymaps_dir)
 
-rules = Qemu()
+        etc_dir = self.builder.j('{install_dir}', self.config['prefix'][1:], 'etc', config=self.config)
+        xyz.rmtree(etc_dir)
+
+        # Copy qemu-system-arm to the right bin location...
+        bin_dir = self.builder.j('{install_dir}', self.config['prefix'][1:], 'bin', config=self.config)
+        ebin_dir = self.builder.j('{install_dir}', self.config['prefix'][1:], '{host}', 'bin', config=self.config)
+        xyz.ensure_dir(ebin_dir)
+        shutil.copy(self.builder.j(bin_dir, 'qemu-system-arm'), ebin_dir)
+        xyz.rmtree(bin_dir)
+
+rules = Qemu
