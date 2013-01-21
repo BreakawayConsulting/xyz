271737b10f2499abe126d5ee5f52a15488aad918
Benno Leslie <benno@breakawayconsulting.com.au>
2013-01-21 16:56:16 +1100

    Added stlink rules

---
+++ rules/stlink.py
@@ -0,0 +1,29 @@
+import xyz
+import os
+import shutil
+
+class Stlink(xyz.BuildProtocol):
+    pkg_name = 'stlink'
+    deps = ['libusb']
+    def configure(self):
+        pass
+
+    def make(self):
+        if self.config['host'].endswith('darwin'):
+            os_ldflags = 'OS_LDFLAGS="-lobjc -Wl,-framework,IOKit -Wl,-framework,CoreFoundation"'
+        else:
+            os_ldflags = ''
+        self.builder.cmd('make', '-f', '{source_dir_from_build}/Makefile',
+                         'LIBUSB_CFLAGS=-I{devtree_dir_abs}/include/libusb-1.0',
+                         'LIBUSB_LDFLAGS=-L{devtree_dir_abs}/{host}/lib',
+                         os_ldflags,
+                         '{jobs}',
+                         config=self.config)
+
+    def install(self):
+        install_dir = self.builder.j('{install_dir_abs}', '{host}', 'bin', config=self.config)
+        with xyz.chdir(self.config['build_dir']):
+            xyz.ensure_dir(install_dir)
+            shutil.copy('st-util', install_dir)
+
+rules = Stlink
