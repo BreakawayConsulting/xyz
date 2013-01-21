da65e041a7d05eee1cdda7c6f5a23ae2fea70103
Benno Leslie <benno@breakawayconsulting.com.au>
2013-01-21 18:19:39 +1100

    Add stlink package to arm toolchain

--- rules/arm-toolchain.py
+++ rules/arm-toolchain.py
@@ -5,7 +5,8 @@ class ArmToolchain(xyz.BuildProtocol):
     pkg_name = 'arm-toolchain'
     deps = [('gcc', {'target': 'arm-none-eabi'}),
             ('binutils', {'target': 'arm-none-eabi'}),
-            ('gdb', {'target': 'arm-none-eabi'})
+            ('gdb', {'target': 'arm-none-eabi'}),
+            ('stlink', {})
             ]
 
 rules = ArmToolchain
